f68ab718ba58a54bd0b4f46362a895c614be2c1b
frederikholst <frederikholstknudsen@gmail.com>
2024-11-15 17:06:33 -0500

    Solver

--- soliton_plot.py
+++ soliton_plot.py
@@ -20,25 +20,28 @@ def soliton_solution(x,t,c=1,x0=0):
     start position x0, float64/float32
  
     Output: 
-    u(x,t), flaot64/float32
+    Solution to KdV for the point x, and t: u(x,t), flaot64/float32
     """
     return -c/2*sech(1/2*np.sqrt(c)*(x-c*t-x0))**2
 
-x_series =np.arange(0,1,100)
-t_series=np.arange(0,6)
-A_series=[soliton_solution(x_series,t) for t in t_series]
+if __name__ == "__main__":
+    # The plots shouldn't be imported with the functions above:
+    x_series =np.arange(0,40,0.5)
+    t_series=np.arange(10,28,3)
+    A_series=[soliton_solution(x_series,t) for t in t_series]
 
 
-fig, axes = plt.subplots(2, 3, figsize=(12, 8))
+    fig, axes = plt.subplots(2, 3, figsize=(12, 8))
 
-# Each subplot of A_series[i] vs. x_series:
-for i, ax in enumerate(axes.flat):
-    ax.plot(x_series, A_series[i],".")  # Plot each A_series element
-    ax.set_title(f'Soliton Solution at t={t_series[i]}')
-    ax.set_xlabel('x')
-    ax.set_ylabel('Amplitude')
+    # Each subplot of A_series[i] vs. x_series:
+    for i, ax in enumerate(axes.flat):
+        ax.plot(x_series, A_series[i],".")  # Plot each A_series element
+        ax.set_title(f'Soliton at time = {t_series[i ]}')
+        ax.set_xlabel('x [A.U.]')
+        ax.set_ylabel('Amplitude [A.U.]')
 
-# To prevent overlap
-plt.tight_layout()
 
-plt.show()
+    # To prevent overlap
+    plt.tight_layout()
+    plt.savefig("Soliton_plot.png")
+    plt.show()
