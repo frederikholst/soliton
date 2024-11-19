9b6a90ed4a7c2bde9d3d2ffd6d87ae9721a2c254
frederikholst <frederikholstknudsen@gmail.com>
2024-11-19 16:53:55 -0500

    quantify

---
+++ Quantify_advection.py
@@ -0,0 +1,48 @@
+from solver import KdV_Solver
+from soliton_plot import soliton_solution
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# Define constants
+Delta_x = 0.2                  # Spatial step size
+X_max = 40                     # System length
+x_size = int(X_max / Delta_x)  # Number of spatial steps
+
+Delta_t = 0.001                # Time step size
+T_max = 20                     # Maximum time
+t_size = int(T_max / Delta_t)  # Number of time steps
+x_series = np.linspace(0, X_max, x_size)  # Positions
+
+# Initialize solution array
+u_gaus_init = np.zeros([x_size, t_size], dtype=float)
+u_soliton_init=np.copy(u_gaus_init)
+# define the gaus initial conditions
+def init(x,mu=X_max/2,sig=1):
+    return (
+        1.0 / (np.sqrt(2.0 * np.pi) * sig) * np.exp(-np.power((x - mu) / sig, 2.0) / 2)
+    )
+
+# Initialize start conditions:
+u_gaus_init[:, 0] = np.array([init(x) for x in x_series]) 
+
+# Define, for comparison, the soliton initial condition
+t_0 = 20                                  # Initial time
+u_soliton_init[:, 0] = np.array([soliton_solution(x, t_0) for x in x_series])  # Soliton at t0
+
+# Solve the KdV equation
+u_solution_sol = KdV_Solver(u_soliton_init, Delta_x, Delta_t, lin=True, mu=0) # Solutions for time up to T_max
+
+u_solution_gaus = KdV_Solver(u_gaus_init, Delta_x, Delta_t, lin=True, mu=0)
+
+eps=-6
+# Extract six sample solutions from each of them:
+T_series=np.linspace(0,3,6)
+u_sol_list=[u_solution_sol[:, int(t / Delta_t)] for t in T_series]
+u_gaus_list=[u_solution_gaus[:, int(t / Delta_t)] for t in T_series]
+u_gaus_ana=[np.array([init(x,X_max/2+t*eps) for x in x_series]) for t in T_series]
+
+
+plt.plot(x_series,u_gaus_list[-2],".")
+plt.plot(x_series,u_gaus_ana[-2])
+plt.show()
---
+++ Quantify_solitons_plot.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+res_list=np.load("Data\\Res_list.npy")
+
+fig,ax = plt.subplots(1,1,figsize=(6,6))
+T_series = np.linspace(0, 18,40)
+
+ax.plot(T_series, res_list,label="Residuals") 
+
+# Axes title
+ax.set_title("Quantifying solver performance at different times")
+
+# Axes label
+ax.set_xlabel('Time', fontsize = 15)
+ax.set_ylabel('Sum of squared Residuals', fontsize = 15)
+
+# Ticks font size
+ax.tick_params(axis='both', labelsize=15)  
+
+# To prevent overlap
+plt.tight_layout()
+plt.savefig("Figures\\res.png")
+plt.show()
